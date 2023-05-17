993ddcee1940be99f3122590347f7093d527b2c9
Andrey V <Andreyxvv@hotmail.com>
2023-05-17 20:10:34 +0500

    first commit

---
+++ HomeWorkCPP4.6.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <utility>
+#include <set>
+
+int main()
+{   
+    std::set<int, std::greater<int>> arr;
+    unsigned int num = 0;
+    int num_arr = 0;
+
+    std::cout << "[IN]: " << std::endl;
+    std::cin >> num;
+    for (int i = 0; i < num; i++) {
+        std::cin >> num_arr;
+        arr.insert(num_arr);
+    }
+
+    std::cout << "[OUT]: " << std::endl;
+    for (auto const& num_arr : arr) {
+        std::cout << num_arr << std::endl;
+    }
+}
